0a0de8ae2dc7865773772ae57d7ca9d78ddca353
nguyenchilinh <nguyenchilinh19992012@gmail.com>
2019-09-28 22:08:18 +0700

    tuan3

---
+++ tuan3/Lab1/src/bai1/JFraame_canban.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai1;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Dell
+ */
+public class JFraame_canban extends JFrame{
+    public static void main(String[] args) {
+        new JFraame_canban();
+        
+        
+            
+    }
+    
+}
