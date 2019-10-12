9edbb1c31c580f2b17c57ecc879d1e3e150ed314
nguyenchilinh <nguyenchilinh19992012@gmail.com>
2019-10-12 22:46:17 +0700

    tuan 4 va tuan 5

---
+++ com.k11DHGD_TTMMT/src/com/k11dhgd_tuan4/ComK11DHGD_tuan4.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.k11dhgd_tuan4;
+
+/**
+ *
+ * @author Dell
+ */
+public class ComK11DHGD_tuan4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ com.k11DHGD_TTMMT/src/com/k11dhgd_tuan5/NhanVien.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.k11dhgd_tuan5;
+
+/**
+ *
+ * @author Dell
+ */
+public class NhanVien {
+    private  int id;
+    private String name;
+    private String address;
+    private String gender;
+    private String knoledge;
+    private String subject;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getKnoledge() {
+        return knoledge;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setKnoledge(String knoledge) {
+        this.knoledge = knoledge;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+    
+    
+    
+}
